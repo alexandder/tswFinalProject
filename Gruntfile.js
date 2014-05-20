96d0bf492175db69a2bd7ead873af8ad848d9801
alexandder <aleksanderbolt@yahoo.com>
2014-05-20 22:21:48 +0200

    setup continue

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		less {
+
+		},
+		watch {
+
+		}
+	});
+};
--- app.js
+++ app.js
@@ -8,6 +8,13 @@ var io = socketIO.listen(httpServer);
 
 app.use(express.static("public"));
 app.use(express.static("bower_components"));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+
+io.sockets.on('connection', function (socket) {
+
+});
+
 
 httpServer.listen(3000, function() {
 	console.log("Server is listening on port 3000...");
